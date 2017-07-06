ef6701b7e67c0ae1609a648ba3db91ad23b158cd
Nicolas Stefani <nicolas471@gmail.com>
2017-07-06 15:34:10 -0300

    basic template for admin

--- consultorio_index/admin.py
+++ consultorio_index/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Member, Rol, Theatre, Genre, Play, Image
 
-# Register your models here.
+admin.site.register(Member)
+admin.site.register(Rol)
+admin.site.register(Theatre)
+admin.site.register(Genre)
+admin.site.register(Play)
+admin.site.register(Image)
