f23aeb5aacbfa38c505d34161ff436ef3c67e329
Nicolas Stefani <nicolas471@gmail.com>
2017-07-07 14:28:32 -0300

    add upload_to arg

--- consultorio_index/models.py
+++ consultorio_index/models.py
@@ -59,7 +59,7 @@ class Play(models.Model):
 
 class Image(models.Model):
     name = models.CharField(max_length=50)
-    image = models.ImageField()
+    image = models.ImageField(upload_to='images')
 
     def __str__(self):
         return self.name
