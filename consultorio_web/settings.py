528f311ac82f76b798903ac3a8f7fe8c02e3b2c2
Nicolas Stefani <nicolas471@gmail.com>
2017-07-07 11:17:21 -0300

    set lang and tz

--- consultorio_web/settings.py
+++ consultorio_web/settings.py
@@ -86,9 +86,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es_ar'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Argentina/Cordoba'
 
 USE_I18N = True
 
