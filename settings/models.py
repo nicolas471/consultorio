c49167eafcf8158b85cc225f1d50b7198cd4d71a
Nicolas Stefani <nicolas471@gmail.com>
2017-07-10 11:58:47 -0300

    add meta options

--- settings/models.py
+++ settings/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 
 class Carrousel(models.Model):
+
+    class Meta():
+        verbose_name_plural = 'Carrousel'
+
     image = models.ImageField(upload_to='carrousel')
