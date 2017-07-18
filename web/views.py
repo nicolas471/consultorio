b24974562362d57bacd7580d58c666bd73efe315
Nicolas Stefani <nicolas471@gmail.com>
2017-07-18 19:11:44 -0300

    migrate legacy views

--- web/views.py
+++ web/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'web/index.html')
+
+def about(request):
+    return render(request, 'web/about.html')
+
+def portfolio(request):
+    return render(request, 'web/portfolio.html')
+
+def contact(request):
+    return render(request, 'web/contacto.html')
