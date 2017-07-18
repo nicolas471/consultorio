fcf9d60a171d5393f83371bd7fe208dacb0bbc56
Nicolas Stefani <nicolas471@gmail.com>
2017-07-18 19:12:51 -0300

    basic url views

--- consultorio/urls.py
+++ consultorio/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from web import views
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'consultorio_web.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.home),
+    url(r'^about', views.about),
+    url(r'^portfolio', views.portfolio),
+    url(r'^contact', views.contact)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
